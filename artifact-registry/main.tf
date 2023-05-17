db531b832cf93a606bb5dc32bee148fbf96d4ebd
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-05-17 09:57:40 +0800

    update location

--- artifact-registry/main.tf
+++ artifact-registry/main.tf
@@ -1,7 +1,6 @@
 resource "google_artifact_registry_repository" "renata-dev-repo" {
-  location      = module.vpc.region
+  location      = var.region
   repository_id = var.repository_id 
-  region        = var.region
   description   = "This is the artifact registry for Backend API service"
   format        = "DOCKER"
   docker_config {
