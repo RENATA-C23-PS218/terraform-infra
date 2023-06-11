fde91885ad5d9a92b086191348e328ed543b5b2c
Gede Rico Wijaya <gricowijaya@gmail.com>
2023-06-11 16:53:32 +0800

    RD-92 remove immutable tags config

--- artifact-registry/main.tf
+++ artifact-registry/main.tf
@@ -4,6 +4,6 @@ resource "google_artifact_registry_repository" "renata-repo" {
   description   = "This is the artifact registry for Backend API service"
   format        = "DOCKER"
   docker_config {
-    immutable_tags = true
+    immutable_tags = false
   }
 }
